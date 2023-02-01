f30c8e72aab0b2e74761fc67eba9f7da15db8207
adril777 <122954464+adril777@users.noreply.github.com>
2023-01-31 23:49:22 -0500

    Add files via upload

---
+++ Deliverable 3 - Iterative Statement/Deliverable 3 - Iterative Statement/Program.cs
@@ -0,0 +1,49 @@
+﻿int max = 0;
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Enter integer between 1 to 100: ");
+        max = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Only integer numbers are allowed");
+    }
+}
+
+// Specify series odd or even
+Console.WriteLine("Specify series (Odd/Even): ");
+// Let user enter series in any input
+string series = Console.ReadLine().ToLower();
+
+// Display the output
+Console.WriteLine("You have selected the ", series, " series. The numbers between 0 and ", max, " are:");
+// If series is odd
+if (series == "odd")
+{
+    // Loop from 1 to max 
+    for (int i = 1; i <= max; i++)
+    {
+        // Check if number is odd
+        if (i % 2 == 1)
+        {
+            // Display the output
+            Console.WriteLine(i);
+        }
+    }
+}
+// If series is even
+if (series == "even")
+{
+    // Loop from 1 to max 
+    for (int i = 1; i <= max; i++)
+    {
+        // Check if number is even
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
